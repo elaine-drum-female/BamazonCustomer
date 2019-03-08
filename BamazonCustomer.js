bf2b882ff96c15a0de6c022bf43341e8953fd1f9
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-03-08 17:40:37 -0600

    Created DisplayProducts function

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -28,3 +28,27 @@ connection.connect(function (err) {
     if (err) throw err;
     runSearch();
 });
+
+// Prompt user by asking what h/she would like to do
+
+function displayProducts(){
+    inquirer.prompt({
+        name:"action",
+        type:"rawlist",
+        message:"What would you like to do?",
+        choices: [ "Choose an item", "Exit"
+        ]
+    }).then(function (answer) {
+        // Another way to do If statements
+        // User will "choose either item" or "exit"
+        switch(answer.action) {
+            case "Choose an item":
+            displayProducts();
+            break;
+
+            case "Exit":
+            connection.end();
+            break;
+        }
+    });
+}
