2ece8596fdfb208d5d49fa58759bc6dae10b982c
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-03-08 17:26:36 -0600

    Connect to the MySQL Database

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,3 +1,22 @@
 // Declare Variables
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+//Create connection to the MySQL Database
+var connection = mysql.createConnection({
+
+    // Your Host
+    host: "localhost",
+
+    // Port Number, if not 3306
+    port: 3306,
+
+    // Username
+    user: "root",
+
+    //Password
+    password: "password",
+
+    //Database
+    database: "bamazon_db"
+});
